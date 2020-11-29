b9bfaf202ac304e5c63bc2611ae05296d53103de
Guilherme Welter <guilherme.a.welter@gmail.com>
2020-11-29 16:39:35 -0300

    Refactor terminal class

--- modulo03/project-esmodule-internationalization/src/index.js
+++ modulo03/project-esmodule-internationalization/src/index.js
@@ -1,27 +1,28 @@
-import DraftLog from "draftlog";
-import chalk from "chalk";
-import chalkTable from "chalk-table";
-import readline from "readline";
-
 import database from "../database.json";
 import { Person } from "./person.js";
+import { TerminalController } from "./terminal.controller.js";
 
-DraftLog(console).addLineListener(process.stdin);
 const DEFAULT_LANG = "pt-BR";
+const STOP_TERM = ":q";
 
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: "id", name: chalk.cyan("ID") },
-    { field: "vehicles", name: chalk.magenta("Vehicles") },
-    { field: "kmTraveled", name: chalk.cyan("Km Traveled") },
-    { field: "from", name: chalk.cyan("From") },
-    { field: "to", name: chalk.cyan("To") },
-  ],
-};
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
 
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
-);
-console.draft(table);
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question("What??");
+    if (answer === STOP_TERM) {
+      terminalController.closeTerminal();
+      console.log("process finished!");
+      return;
+    }
+    const person = Person.generateInstanceFromString(answer);
+    console.log(person.formatted(DEFAULT_LANG));
+    return mainLoop();
+  } catch (error) {
+    console.error("Deu Ruim", error);
+    return mainLoop();
+  }
+}
+
+await mainLoop();
